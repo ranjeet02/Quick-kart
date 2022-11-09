20d46ad8d018d72c482ce1acea733d790bfe6d41
Ranjeet kumar jha <106006987+ranjeet02@users.noreply.github.com>
2022-11-09 14:39:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        document.getElementById("top").style.fontSize = "15px";
+    } else {
+        document.getElementById("top").style.fontSize = "20px";
+    }
+}
+//this code is for signup
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+//this code is for login
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
